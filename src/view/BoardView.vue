ee8aa0a19364f1f43d9410e856321275f8575f47
Lenny <ly2484579947@gmail.com>
2024-11-08 16:34:58 +0800

    basic feature completed

--- src/view/BoardView.vue
+++ src/view/BoardView.vue
@@ -1,6 +1,15 @@
 <template>
   <!-- 指令可以直接写在组件上, 但是事件不可以 -->
   <div class="board">
+    <div class="tips">
+      {{
+        boardContent.filter((item) => item !== null).length === boardContent.length
+          ? `Game Over, Please Click the Restart Button`
+          : winner
+            ? `The Winner is: ${winner}`
+            : `The Next Player is: ${isX ? 'X' : 'O'}`
+      }}
+    </div>
     <div class="board-row" v-for="i in 3" :key="i">
       <BoardItem
         v-for="j in 3"
@@ -15,13 +24,14 @@
 
 <script setup lang="ts">
 import BoardItem from '@/components/BoardItem.vue'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 const boardContent = ref(Array(9).fill(null))
+// isX可以优化吗
 const isX = ref(true)
 
 const handleItemClick = (index: number) => {
   // 如果有值 不能再点击赋值
-  if (boardContent.value[index]) return
+  if (boardContent.value[index] || winner.value) return
   boardContent.value[index] = isX.value ? 'X' : 'O'
   isX.value = !isX.value
 }
@@ -29,10 +39,11 @@ const handleRestartClick = () => {
   boardContent.value = Array(9).fill(null)
 }
 
+const winner = computed(() => calcWinner(boardContent.value, isX.value ? 'O' : 'X'))
 type CType = 'X' | 'O'
 // 计算当前是否有获胜者
 const calcWinner = (board: (string | null)[], currentC: CType) => {
-  const winnerType = [
+  const winnerTypes = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -42,6 +53,14 @@ const calcWinner = (board: (string | null)[], currentC: CType) => {
     [0, 4, 8],
     [2, 4, 6],
   ]
+  for (const [one, two, three] of winnerTypes) {
+    if (board[one] === currentC && board[two] === currentC && board[three] === currentC) {
+      console.log(one)
+
+      return currentC
+    }
+  }
+  return null
 }
 </script>
 
@@ -52,6 +71,10 @@ const calcWinner = (board: (string | null)[], currentC: CType) => {
   justify-content: center;
   align-items: center;
   text-align: center;
+  .tips {
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
   .board-row {
     display: flex;
   }
